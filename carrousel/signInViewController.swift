be6646d1459a7d1910b6600968a1050724623f2e
Alexispicard <mellis1@L-SB862E2DRJ-M.local>
2015-10-14 18:03:55 -0700

    Scroll View still not working Issue to copy navigation bar as Image view

---
+++ carrousel/signInViewController.swift
@@ -0,0 +1,35 @@
+//
+//  signInViewController.swift
+//  carrousel
+//
+//  Created by Marcus Ellison - Vendor on 10/14/15.
+//  Copyright © 2015 Alexis Picard. All rights reserved.
+//
+
+import UIKit
+
+class signInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
